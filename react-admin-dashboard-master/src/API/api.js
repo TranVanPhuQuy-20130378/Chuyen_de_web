7dd934653c24a77d3b9df65c3a7e4ca8230bdc1b
TranVanPhuQuy-20130378 <tranvanphuquy01266984818@gmail.com>
2024-06-19 14:20:44 +0700

    function admin

---
+++ react-admin-dashboard-master/src/API/api.js
@@ -0,0 +1,30 @@
+export async function getUsers() {
+    try {
+        const response = await fetch('http://localhost:8080/api/users');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+}
+export async function deleteUser(id) {
+    try {
+      const response = await fetch(`http://localhost:8080/api/users/${id}`, {
+        method: 'DELETE',
+      });
+      
+      if (!response.ok) {
+        const errorDetails = await response.json(); // Xử lý JSON khi có lỗi
+        console.error('Failed to delete user:', errorDetails);
+        throw new Error('Failed to delete user');
+      }
+  
+    
+      
+    } catch (error) {
+      console.error('Error:', error);
+      throw error;
+    }
+  }
+  
